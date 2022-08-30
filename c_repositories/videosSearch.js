7a980f820255c67bb24be6ff51987110935d6204
winmin90 <108992367+winmin90@users.noreply.github.com>
2022-08-30 20:32:27 +0900

    add repositories video

--- c_repositories/videosSearch.js
+++ c_repositories/videosSearch.js
@@ -1,7 +1,22 @@
-const VideoSearchs = require("../d_schemas/videoSearch");
+// const videoSearch = require("../d_schemas/videoSearch");
+const VideoSearch = require("../d_schemas/videoSearch");
 
+class VideosSearchRepository {
+  getVideoSearch = async (_id) => {
+    const thisVideo = await VideoSearch.findOne({ where: { _id:_id } });
+    return thisVideo;
+  };
+  
+  createVideoSearch = async ( video_id, title, description) => {
+    const createdVideoSearch = await VideoSearch.create({
+      video_id,
+      title,
+      description,
+    });
 
-
-
+    return createdVideoSearch;
+  };
+};
 
 module.exports = VideosSearchRepository;
+1
