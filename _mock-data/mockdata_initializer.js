bf1a81df46df754b6d865dbf09b90eb3ca2bbb4c
김보현 <bohyeonkim@gimbohyeon-ui-MacBook-Pro.local>
2022-08-31 10:33:52 +0900

    Add mock data sets

---
+++ _mock-data/mockdata_initializer.js
@@ -0,0 +1,17 @@
+const {
+  users,
+  collections,
+  comments,
+  categories,
+  videos,
+} = require("../main-data-in.js");
+
+const Users = require("../d_schemas/user");
+const Collections = require("../d_schemas/collection");
+const Comments = require("../d_schemas/comment");
+const Categories = require("../d_schemas/category");
+const Videos = require("../d_schemas/video");
+
+const dataInitializer = () => {};
+
+module.exports = dataInitializer;
--- a_routes/index.js
+++ a_routes/index.js
@@ -1,28 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const authRouter = require("./auth.routes.js");
-const Users = require("../d_schemas/user")
+const Users = require("../d_schemas/user");
 
 router.use("/", [authRouter]);
 router.use("/DBtest", async (req, res) => {
+  await Users.create({
+    user_id: "tester",
+    email: "tester@test.com",
+    firstName: "bohyeon",
+    lastName: "kim",
+    profilePicUrl:
+      "https://t4.ftcdn.net/jpg/03/40/12/49/360_F_340124934_bz3pQTLrdFpH92ekknuaTHy8JuXgG7fi.jpg",
+    myCollectionsArr: [],
+    likesArr: [],
+    follwersArr: [],
+    followingsArr: [],
+    displayName: "Tester1",
+    createdAt: new Date(),
+  });
 
-    await Users.create({  
-      user_id: "tester",
-      email: "tester@test.com",
-      firstName: "bohyeon",
-      lastName: "kim",
-      profilePicUrl: 
-          "https://t4.ftcdn.net/jpg/03/40/12/49/360_F_340124934_bz3pQTLrdFpH92ekknuaTHy8JuXgG7fi.jpg",
-      myCollectionsArr: [],
-      likesArr: [],
-      follwersArr: [],
-      followingsArr: [],
-      displayName: "Tester1",
-      createdAt: new Date(),
-        })
-
-      res.redirect("/")
-
+  res.redirect("/");
 });
 
 // 이 파일에서 만든 router 객체를 외부에 공개 -> app.js에서 사용할 수 있도록
