3ed752efc169b99a72d6c9e652dc4c6a5b64f357
winmin90 <108992367+winmin90@users.noreply.github.com>
2022-09-01 14:53:44 +0900

    add routes commnt 추가
    routes comment 추가
    service comment 추가
    comment test 추가
    service category 추가

--- a_routes/index.js
+++ a_routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authRouter = require("./auth.routes.js");
 const Users = require("../d_schemas/user")
+const Comments = require("../d_schemas/comment")
 
 router.use("/", [authRouter]);
 router.use("/DBtest", async (req, res) => {
@@ -23,6 +24,6 @@ router.use("/DBtest", async (req, res) => {
       res.redirect("/")
 
 });
-
+router.use("/comments", [Comments]);
 // 이 파일에서 만든 router 객체를 외부에 공개 -> app.js에서 사용할 수 있도록
 module.exports = router;
