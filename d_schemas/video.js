bb891aa4d540a2a3b2654bb47c61420f21a4d909
김보현 <bohyeonkim@gimbohyeon-ui-MacBook-Pro.local>
2022-09-01 15:35:34 +0900

    feature to origin feature

--- d_schemas/video.js
+++ d_schemas/video.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const VideoSchema = new mongoose.Schema({
-<<<<<<< HEAD
   videoTitle: {
     type: String,
     required: true,
@@ -26,46 +25,12 @@ const VideoSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+
   createdAt: {
-    type: String,
+    type: Date,
+    default: Date.now,
     required: true,
   },
-=======
-    _id: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    videoTitle: {
-        type: String,
-        required: true,        
-      },
-    channelTitle: {
-        type: String,
-        required: true,        
-      },
-    channel_id: {
-        type: String,
-        required: true,       
-      },
-    category_id: {
-        type: String,
-        required: true,        
-      },
-    thumbnails: {
-        type: String,
-        required: true,        
-      },
-    description: {
-        type: String,
-        required: true,        
-      },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        required: true,
-      },
->>>>>>> feature
 });
 
 module.exports = mongoose.model("Videos", VideoSchema);
