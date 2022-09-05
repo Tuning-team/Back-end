fa226778eb2b242d6acca692a001636fc7e91c99
winmin90 <108992367+winmin90@users.noreply.github.com>
2022-09-05 12:00:51 +0900

    change auth.routes some

--- a_routes/auth.routes.js
+++ a_routes/auth.routes.js
@@ -4,18 +4,18 @@ const axios = require("axios");
 const passport = require("passport");
 const UserRepository = require("../c_repositories/users.repository");
 const userRepository = new UserRepository();
-// const session = require("express-session");
+const session = require("express-session");
 
 // 세션ID를 가진 사용자가 접속했을 때,유저 정보 받아보기
 router.get("/user", async (req, res) => {
-  if (req.session.passport) {
-    console.log(
-      "여기에" +
-        req.session.passport.user.user.displayName +
-        "님이 접속했습니다."
-    );
-    res.json(req.session.passport.user);
-  } 
+//   if (req.session.passport) {
+//     console.log(
+//       "여기에" +
+//         req.session.passport.user.user.displayName +
+//         "님이 접속했습니다."
+//     );
+//     res.json(req.session.passport.user);
+//   } 
 
   const user_id = process.env.TEMP_USER_ID;
   const user = await userRepository.getUserById(user_id);
