e755a10b932966a0aec9c278f5673d2f3bdbd836
김보현 <bohyeonkim@gimbohyeon-ui-MacBook-Pro.local>
2022-09-08 21:43:39 +0900

    Make Category schema string to array

--- _mock-data/mockdata_initializer.js
+++ _mock-data/mockdata_initializer.js
@@ -5,15 +5,15 @@ connect();
 
 const {
   // users,
-  // collections,
-  comments,
+  collections,
+  // comments,
   // videos,
   // categories,
 } = require("./main-data-in_wo_ids.js");
 
 // const Users = require("../d_schemas/user");
-// const Collections = require("../d_schemas/collection");
-const Comments = require("../d_schemas/comment");
+const Collections = require("../d_schemas/collection");
+// const Comments = require("../d_schemas/comment");
 // const Categories = require("../d_schemas/category");
 // const Videos = require("../d_schemas/video");
 
@@ -24,8 +24,8 @@ const Comments = require("../d_schemas/comment");
 // console.log("videos", videos);
 
 try {
-  // Collections.create(collections).then((e) => console.log(e)); // 완료
-  Comments.create(comments).then((e) => console.log(e));
+  Collections.create(collections).then((e) => console.log(e)); // 완료
+  // Comments.create(comments).then((e) => console.log(e));
   // Categories.create(categories).then((e) => console.log(e)); // 완료
   // Videos.create(videos).then((e) => console.log(e)); // 완료
 } catch (error) {
@@ -33,7 +33,7 @@ try {
 }
 
 // const dataInitializer = () => {
-//   const createdCollections = Collections.create(collections);
+// const createdCollections = Collections.create(collections);
 //   const createdComments = Comments.create(comments);
 //   const createdCategories = Categories.create(categories);
 //   const createdVideos = Videos.create(videos);
