84ecaac11c16250c8d38811a473c30934682fdbe
김보현 <bohyeonkim@gimbohyeon-ui-MacBook-Pro.local>
2022-09-23 10:59:09 +0900

    Edit interestedCategories API

--- b_services/users.service.js
+++ b_services/users.service.js
@@ -28,13 +28,13 @@ class UserService {
       let { category_id } = req.params;
       let category_ids = category_id.split(",").map((e) => e.trim()); // 배열
 
-      const user_id = res.locals.user_id;
-      // const user_id = process.env.TEMP_USER_ID;
+      // const user_id = res.locals.user_id;
+      const user_id = process.env.TEMP_USER_ID;
 
-      let { myInterestingCategories } = await this.usersRepository.getUserById(user_id);
-      myInterestingCategories = Array.from(new Set([...myInterestingCategories, ...category_ids]));
+      // let { myInterestingCategories } = await this.usersRepository.getUserById(user_id);
+      // myInterestingCategories = Array.from(new Set([...category_ids]));
 
-      await this.usersRepository.setInterestCategories(user_id, myInterestingCategories);
+      await this.usersRepository.setInterestCategories(user_id, category_ids);
 
       let categories = await this.categoryRepository.getAllCategories(category_ids);
 
